813b60b4c8ec201e2a87dc27c8553d9f770835d7
笹倉直哉 <a_20100409@yahoo.co.jp>
2025-09-02 22:34:31 +0900

    Google Custom Seacherを使用するサービスクラスを追加

---
+++ Experience2Notion/Services/GoogleImageSearcher.cs
@@ -0,0 +1,21 @@
+﻿using Google.Apis.CustomSearchAPI.v1;
+using Google.Apis.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Experience2Notion.Services;
+public class GoogleImageSearcher
+{
+    public GoogleImageSearcher()
+    {
+        var apiKey = Environment.GetEnvironmentVariable("GOOGLE_CUSTOM_SEARCH_API_KEY");
+        var searcher = new CustomSearchAPIService(new BaseClientService.Initializer
+        {
+            ApiKey = apiKey,
+            ApplicationName = "Experience2Notion"
+        });
+    }
+}
