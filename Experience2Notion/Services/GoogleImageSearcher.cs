a66bd555f1f43cca74e74244c6aa1a4223563aad
笹倉直哉 <a_20100409@yahoo.co.jp>
2025-09-02 23:08:47 +0900

    画像のリンクを取得する処理を実装

--- Experience2Notion/Services/GoogleImageSearcher.cs
+++ Experience2Notion/Services/GoogleImageSearcher.cs
@@ -1,21 +1,31 @@
 ﻿using Google.Apis.CustomSearchAPI.v1;
 using Google.Apis.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Experience2Notion.Services;
 public class GoogleImageSearcher
 {
+    readonly CustomSearchAPIService _searcher;
+    readonly string _cx;
+
     public GoogleImageSearcher()
     {
         var apiKey = Environment.GetEnvironmentVariable("GOOGLE_CUSTOM_SEARCH_API_KEY");
-        var searcher = new CustomSearchAPIService(new BaseClientService.Initializer
+        _searcher = new CustomSearchAPIService(new BaseClientService.Initializer
         {
             ApiKey = apiKey,
             ApplicationName = "Experience2Notion"
         });
+        _cx = Environment.GetEnvironmentVariable("GOOGLE_CUSTOM_SEARCH_ENGINE_ID") ?? throw new ArgumentException("Google Custom Searchの検索エンジンIDが指定されていません。");
+    }
+
+    public async Task<string?> SearchImageAsync(string query)
+    {
+        var listRequest = _searcher.Cse.List();
+        listRequest.Q = query;
+        listRequest.Cx = _cx;
+        listRequest.SearchType = CseResource.ListRequest.SearchTypeEnum.Image;
+        listRequest.Num = 1;
+        var search = await listRequest.ExecuteAsync();
+        return search.Items?.FirstOrDefault()?.Link;
     }
 }
