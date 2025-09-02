27a6197fa2de5ccc72df9a8679f4530baa1ac9ac
笹倉直哉 <a_20100409@yahoo.co.jp>
2025-09-02 23:40:59 +0900

    FileUploadのレスポンスを表すクラスを追加

---
+++ Experience2Notion/Models/Notions/NotionFileUploadResponse.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Experience2Notion.Models.Notions;
+public class NotionFileUploadResponse
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; } = string.Empty;
+}
