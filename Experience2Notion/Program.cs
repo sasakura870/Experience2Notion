21c4d5042dbba159b1a34e32466790ef5939c101
笹倉直哉 <a_20100409@yahoo.co.jp>
2025-09-04 22:07:09 +0900

    Google Booksからデータを取得するサービスクラスを追加

--- Experience2Notion/Program.cs
+++ Experience2Notion/Program.cs
@@ -1,16 +1,7 @@
-﻿using Google.Apis.Books.v1;
-using Experience2Notion.Services;
+﻿using Experience2Notion.Services;
 
-var service = new BooksService();
-var request = service.Volumes.List($"isbn:{args[0]}");
-var response = await request.ExecuteAsync();
-if (response.Items is null || response.Items.Count == 0)
-{
-    Console.WriteLine("指定された本が見つかりませんでした。");
-    return;
-}
-
-var book = response.Items[0].VolumeInfo;
+var service = new GoogleBookSeacher();
+var book = await service.SearchByIsbnAsync(args[0]);
 
 Console.WriteLine($"タイトル: {book.Title}");
 Console.WriteLine($"著者: {string.Join(", ", book.Authors)}");
---
+++ Experience2Notion/Services/GoogleBookSeacher.cs
@@ -0,0 +1,19 @@
+﻿using Google.Apis.Books.v1;
+using Google.Apis.Books.v1.Data;
+
+namespace Experience2Notion.Services;
+public class GoogleBookSeacher
+{
+    readonly BooksService _service = new();
+
+    public async Task<Volume.VolumeInfoData> SearchByIsbnAsync(string isbn)
+    {
+        var request = _service.Volumes.List($"isbn:{isbn}");
+        var response = await request.ExecuteAsync();
+        if (response.Items is null || response.Items.Count == 0)
+        {
+            throw new Exception("指定された本が見つかりませんでした。");
+        }
+        return response.Items[0].VolumeInfo;
+    }
+}
