6d877ff80828bf2a0998988b2afef2b316f19065
笹倉直哉 <a_20100409@yahoo.co.jp>
2025-09-09 17:45:28 +0900

    このアプリに関係するExceptionクラスを追加

---
+++ Experience2Notion/Exceptions/Experience2NotionException.cs
@@ -0,0 +1,11 @@
+﻿namespace Experience2Notion.Exceptions;
+[Serializable]
+public class Experience2NotionException : Exception
+{
+	public Experience2NotionException() { }
+	public Experience2NotionException(string message) : base(message) { }
+	public Experience2NotionException(string message, Exception inner) : base(message, inner) { }
+	protected Experience2NotionException(
+	  System.Runtime.Serialization.SerializationInfo info,
+	  System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+}
