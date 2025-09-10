ddf8f86706be59231668ee72c570a579d0f1d921
笹倉直哉 <a_20100409@yahoo.co.jp>
2025-09-10 23:16:18 +0900

    処理を共通化

--- Experience2Notion/Services/NotionClient.cs
+++ Experience2Notion/Services/NotionClient.cs
@@ -58,9 +58,52 @@ public partial class NotionClient
 
     public async Task<CreatePageResponse> CreateBookPageAsync(string title, IList<string> authors, string link, string publishedDate, string imageId)
     {
-        _logger.LogInformation($"Notionにページを作成します。");
-        var bookGenre = _genres.First(g => g.Name == "書籍");
+        _logger.LogInformation($"Notionに書籍ページを作成します。");
+        var payload = CreateNotionPagePayload(title, "書籍", [.. authors], link, publishedDate, imageId);
+        var jsonPayload = JsonSerializer.Serialize(payload);
+        var content = new StringContent(jsonPayload, Encoding.UTF8, MediaTypeNames.Application.Json);
+        var response = await _client.PostAsync(_createPagesUrl, content);
+        response.EnsureSuccessStatusCode();
+        _logger.LogInformation($"Notionのページを作成しました。");
+        _logger.LogInformation("タイトル: {Title}", title);
+        _logger.LogInformation("著者: {Authors}", string.Join(", ", authors));
+        _logger.LogInformation("リンク: {Link}", link);
+        if (DatetimeRegex().IsMatch(publishedDate))
+        {
+            _logger.LogInformation("発売日: {PublishedDate}", publishedDate);
+        }
+        var jsonRes = await response.Content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<CreatePageResponse>(jsonRes)!;
+    }
+
+    public async Task<CreatePageResponse> CreateMusicAlbumPageAsync(string title, string artist, string link, string releaseDate, string imageId)
+    {
+        _logger.LogInformation($"Notionに音楽アルバムページを作成します。");
+        var payload = CreateNotionPagePayload(title, "音楽アルバム", [artist], link, releaseDate, imageId);
+        var jsonPayload = JsonSerializer.Serialize(payload);
+        var content = new StringContent(jsonPayload, Encoding.UTF8, MediaTypeNames.Application.Json);
+        var response = await _client.PostAsync(_createPagesUrl, content);
+        response.EnsureSuccessStatusCode();
+        _logger.LogInformation($"Notionのページを作成しました。");
+        _logger.LogInformation("タイトル: {Title}", title);
+        _logger.LogInformation("アーティスト {Artist}", artist);
+        _logger.LogInformation("リンク: {Link}", link);
+        _logger.LogInformation("発売日: {ReleaseDate}", releaseDate);
+        var jsonRes = await response.Content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<CreatePageResponse>(jsonRes)!;
+    }
+
+    private NotionPageCreate CreateNotionPagePayload(string title, string genre, string[] authors, string link, string publishedDate, string imageId)
+    {
+        var genreOption = _genres.First(g => g.Name == genre);
+        var IconUrl = genre switch
+        {
+            "書籍" => Consts.BookIconUrl,
+            "音楽アルバム" => Consts.MusicIconUrl,
+            _ => ""
+        };
         var authorOptions = authors.Select(author => _authors.FirstOrDefault(a => a.Name == author) ?? new SelectOption { Name = author }).ToList();
+
         var payload = new NotionPageCreate
         {
             Parent = new Parent { DatabaseId = _databaseId },
@@ -69,7 +112,7 @@ public partial class NotionClient
                 Type = "external",
                 External = new ExternalFile
                 {
-                    Url = Consts.BookIconUrl
+                    Url = IconUrl
                 }
             },
             Properties = new PageProperties
@@ -97,7 +140,7 @@ public partial class NotionClient
                 },
                 Genre = new SelectValueByPage
                 {
-                    Select = bookGenre
+                    Select = genreOption
                 },
             },
             Children =
@@ -129,103 +172,7 @@ public partial class NotionClient
                 }
             };
         }
-        var jsonPayload = JsonSerializer.Serialize(payload);
-        var content = new StringContent(jsonPayload, Encoding.UTF8, MediaTypeNames.Application.Json);
-        var response = await _client.PostAsync(_createPagesUrl, content);
-        response.EnsureSuccessStatusCode();
-        _logger.LogInformation($"Notionのページを作成しました。");
-        _logger.LogInformation("タイトル: {Title}", title);
-        _logger.LogInformation("著者: {Authors}", string.Join(", ", authors));
-        _logger.LogInformation("リンク: {Link}", link);
-        if (DatetimeRegex().IsMatch(publishedDate))
-        {
-            _logger.LogInformation("発売日: {PublishedDate}", publishedDate);
-        }
-        var jsonRes = await response.Content.ReadAsStringAsync();
-        return JsonSerializer.Deserialize<CreatePageResponse>(jsonRes)!;
-    }
-
-    public async Task<CreatePageResponse> CreateMusicAlbumPageAsync(string title, string artist, string link, string releaseDate, string imageId)
-    {
-        _logger.LogInformation($"Notionにページを作成します。");
-        var genre = _genres.First(g => g.Name == "音楽アルバム");
-        var authorOptions = _authors.FirstOrDefault(a => a.Name == artist) ?? new SelectOption { Name = artist };
-        var payload = new NotionPageCreate
-        {
-            Parent = new Parent { DatabaseId = _databaseId },
-            Icon = new Icon
-            {
-                Type = "external",
-                External = new ExternalFile
-                {
-                    Url = Consts.MusicIconUrl
-                }
-            },
-            Properties = new PageProperties
-            {
-                Title = new TitleProperty
-                {
-                    Title = [
-                        new TextObject
-                        {
-                            Text = new TextContent { Content = title }
-                        }
-                    ]
-                },
-                Authors = new MultiSelectValueByPage
-                {
-                    MultiSelect = [authorOptions]
-                },
-                Link = new UrlValueByPage
-                {
-                    Url = link
-                },
-                Status = new StatusValueByPage
-                {
-                    Status = _notStartStatus
-                },
-                Genre = new SelectValueByPage
-                {
-                    Select = genre
-                },
-                PublishedDate = new DateValueByPage
-                {
-                    Date = new DateValue
-                    {
-                        Start = releaseDate
-                    }
-                }
-            },
-            Children =
-            [
-                new ParagraphBlock {
-                    Paragraph = new ParagraphContent{
-                        RichText = []
-                    }
-                },
-                new ImageBlock
-                {
-                    Image = new ImageContent
-                    {
-                        Type = "file_upload",
-                        FileUpload = new FileUploadContent{
-                            Id = imageId
-                        }
-                    }
-                }
-            ]
-        };
-        var jsonPayload = JsonSerializer.Serialize(payload);
-        var content = new StringContent(jsonPayload, Encoding.UTF8, MediaTypeNames.Application.Json);
-        var response = await _client.PostAsync(_createPagesUrl, content);
-        response.EnsureSuccessStatusCode();
-        _logger.LogInformation($"Notionのページを作成しました。");
-        _logger.LogInformation("タイトル: {Title}", title);
-        _logger.LogInformation("アーティスト {Artist}", artist);
-        _logger.LogInformation("リンク: {Link}", link);
-        _logger.LogInformation("発売日: {ReleaseDate}", releaseDate);
-        var jsonRes = await response.Content.ReadAsStringAsync();
-        return JsonSerializer.Deserialize<CreatePageResponse>(jsonRes)!;
+        return payload;
     }
 
     private void LoadProperties()
